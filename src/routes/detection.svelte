056deeb0e95610ce6c2cf24dfb97890767f775d8
AlessioToniolo <atoniolo76@gmail.com>
2022-12-03 00:02:18 -0500

    Finished Detection (todo: styling + redirect)

--- src/routes/detection.svelte
+++ src/routes/detection.svelte
@@ -1,36 +1,24 @@
 <script>
 	import papaparse from 'papaparse';
 
-	async function loadCSVData() {
-		await fetch('https://bird-ml-server.vercel.app/api/listOfBirdSpecies.csv', {
-			method: 'GET'
-		}).then((res) => {
-			/*
-			const parseStream = papaparse.parse(papaparse.NODE_STREAM_INPUT);
-			res.body.pipe(parseStream);
-
-			let data = [];
-			parseStream.on('data', (row) => {
-				data.push(row);
-			});
-			parseStream.on('end', () => {
-				console.log(data);
-			});
-
-			console.log(res.body);
-			return res;
-			*/
-		});
-	}
+	let showCamera = true;
+	let showDetection = false;
+	let showTakePictureButton = false;
+	let showPredictButton = false;
+	let showVideoSource = false;
+	let showCanvasSource = false;
 
 	// TODO: machine learning section
 	import * as tf from '@tensorflow/tfjs';
 
 	let videoSource = null;
 	let loading = false;
+	let secondLoading = false;
 
 	const obtainVideoCamera = async () => {
+		showVideoSource = true;
 		try {
+			showCamera = false;
 			loading = true;
 			const stream = await navigator.mediaDevices.getUserMedia({
 				video: true
@@ -38,39 +26,45 @@
 			videoSource.srcObject = stream;
 			videoSource.play();
 			loading = false;
+			showTakePictureButton = true;
 		} catch (error) {
 			console.log(error);
 		}
 	};
 
 	let image;
+	let imageCanvas;
 	//let container;
 	// Webcam promise return
 	async function loadImage() {
+		showCanvasSource = true;
+		loading = true;
 		const camera = await tf.data.webcam(videoSource);
 		// Image camera capture promise return.
 		image = await camera.capture();
+		showTakePictureButton = false;
+		showCamera = false;
+		showVideoSource = false;
+		await tf.browser.toPixels(image, imageCanvas);
 		image = tf.expandDims(tf.image.resizeBilinear(image, [150, 150]).div(tf.scalar(255)), 0);
-		image.print();
-		/*
-		container = document.createElement('canvas');
-		// Stopping the camera.
-		camera.stop();
-		*/
+		loading = false;
+		showPredictButton = true;
 	}
 
 	// Load and process model
 	let model = undefined;
 	let prediction;
+	let accuracy;
 
 	// TODO test code
 	async function predictWebcam() {
+		showPredictButton = false;
+		secondLoading = true;
 		tf.loadLayersModel('https://bird-ml-server.vercel.app/api/model.json').then((loadedModel) => {
 			model = loadedModel;
 			model.summary();
-			prediction = model.predict(image);
-			const predictionArray = prediction.data().then((arr) => {
-				console.log(arr);
+			const predictionData = model.predict(image);
+			const predictionArray = predictionData.data().then((arr) => {
 				let max = arr[0];
 				let maxIndex = 0;
 				for (let i = 0; i < arr.length; i++) {
@@ -79,37 +73,82 @@
 						max = arr[i];
 					}
 				}
-				console.log(maxIndex);
-				console.log(max * 100);
-
-				/*
-				loadCSVData().then((arrOfSpecies) => {
-					console.log(arrOfSpecies);
-				});
-				*/
+				accuracy = Math.round(max * 100);
 				papaparse.parse('https://bird-ml-server.vercel.app/api/listOfBirdSpecies.csv', {
 					download: true,
 					complete: (results) => {
-						console.log(results.data[maxIndex][0]);
+						prediction = results.data[maxIndex][0];
 					}
 				});
+				secondLoading = false;
+				showDetection = true;
 			});
 		});
 	}
 </script>
 
-<div>
-	{#if loading}
-		<h1>LOADING</h1>
+<div
+	class="w-max mt-12 ml-auto mr-auto flex-initial justify-center content-center px-12 text-center py-2"
+>
+	<div class="text-center justify-center">
+		{#if loading}
+			<div class="mt-12 alert alert-warning shadow-lg">
+				<div>
+					<strong>LOADING...</strong>
+				</div>
+			</div>
+		{/if}
+		<!-- svelte-ignore a11y-media-has-caption -->
+		{#if showVideoSource}
+			<video bind:this={videoSource} />
+		{/if}
+		{#if showCanvasSource}
+			<canvas bind:this={imageCanvas} />
+		{/if}
+		{#if showCamera}
+			<div class="card w-96 bg-base-100 shadow-xl">
+				<figure><img src="../../static/hundredpicture.jpg" alt="Shoes" /></figure>
+				<div class="card-body">
+					<h2 class="card-title">Identify Bird Species</h2>
+					<p class="text-left">
+						BSABirds will automatically identify a bird picture taken on your camera!
+					</p>
+					<div class="card-actions justify-center pt-2">
+						<button class="btn-primary btn" on:click={obtainVideoCamera}>Turn on Camera</button>
+					</div>
+				</div>
+			</div>
+		{/if}
+	</div>
+	<div>
+		{#if showTakePictureButton}
+			<button class="btn mt-2" on:click={loadImage}>Take a picture</button>
+		{/if}
+		{#if showPredictButton}
+			<button class="btn mt-2" on:click={predictWebcam}>Predict Bird</button>
+		{/if}
+		{#if secondLoading}
+			<div class="mt-12 alert alert-warning shadow-lg">
+				<div>
+					<strong>LOADING...</strong>
+				</div>
+			</div>
+		{/if}
+	</div>
+	{#if showDetection}
+		<div class="text-center justify-center ml-auto mr-auto mt-8 card w-96 bg-base-100 shadow-xl">
+			<div class="card w-96 bg-secondary">
+				<div class="card-body items-center text-center">
+					<h2 class="card-title">Bird Species:</h2>
+					<p><b>{prediction}</b> with {accuracy}% certainty!</p>
+					<div class="mt-2 space-x-1 card-actions justify-end">
+						<button class="btn btn-primary">Log Bird</button>
+						<a href="/">
+							<button class="btn btn-neutral">Try Again</button>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
 	{/if}
-	<!-- svelte-ignore a11y-media-has-caption -->
-	<video bind:this={videoSource} />
-	<button on:click={obtainVideoCamera}>CLICK</button>
 </div>
-<div>
-	<button class="btn" on:click={loadImage}>Take a picture</button>
-	<button class="btn-primary" on:click={predictWebcam}>Predict Bird</button>
-</div>
-<h1>{prediction}</h1>
-<!--<div bind:this={container} id="resized-image"></div>-->
-<button on:click={loadCSVData}>load csv data</button>
