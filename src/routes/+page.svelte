f90e394e52af2372ed064db99253ce74d3603877
AlessioToniolo <atoniolo76@gmail.com>
2022-12-09 22:55:01 -0500

    Updated SvelteKit Environment with adapter-auto

---
+++ src/routes/+page.svelte
@@ -0,0 +1,67 @@
+<script>
+	import Layout from './+layout.svelte';
+</script>
+
+<div class="container mx-auto min-width-max flex justify-center">
+	<div class="flex flex-wrap justify-center">
+		<div class="text-center">
+			<div class="sm:basis-1/2 text-center mb-5 pt-10 ">
+				<a href="/auto"
+					><p class="text-primary font-extrabold text-3xl">Track Brookhaven Birds</p></a
+				>
+			</div>
+			<div class="mb-10"><a class="btn btn-primary">Get started</a></div>
+		</div>
+		<div class="container min-width-max x flex justify-center py-10">
+			<div class="card w-80 bg-base-100 shadow-xl">
+				<figure><img src="whitebird.jpeg" alt="White Bird" /></figure>
+				<div class="card-body">
+					<p class="card-title">About BSABirds</p>
+					<p>
+						<span class="font-semibold">BSABirds</span> provides data for bird species populations
+						in
+						<a href="https://www.brookhavenga.gov/" class="underline hover:text-primary"
+							>Brookhaven, Georgia</a
+						> by recording sightings of birds in parks.
+					</p>
+					<div class="card-actions justify-end">
+						<button class="btn btn-primary">Track Birds</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container min-width-max flex justify-center py-10">
+			<div class="card w-80 bg-base-100 shadow-xl image-full">
+				<figure><img src="murphey.jpeg" alt="Shoes" /></figure>
+				<div class="card-body">
+					<h2 class="card-title">How it works</h2>
+					<p>
+						BSABirds allows users to log birds by using an automatic bird classification through a
+						phone camera or logging the species manually.
+					</p>
+					<div class="card-actions justify-end">
+						<button class="btn btn-tertiary">Learn More</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container min-width-max flex justify-center py-10">
+			<div class="card w-80 bg-base-100 shadow-xl">
+				<figure><img src="motmot.jpeg" alt="Motmot" /></figure>
+				<div class="card-body">
+					<h2 class="card-title">Participating parks</h2>
+					<p>
+						Visitors to Brookhaven parks can either scan the QR Code on the park's entrance
+						flyer/sign or visit <a
+							href="https://bsabirds.herokuapp.com"
+							class="underline hover:text-primary">this site</a
+						>.
+					</p>
+					<div class="card-actions justify-end">
+						<button class="btn btn-secondary">View Parks</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ src/routes/auto/+page.svelte
@@ -0,0 +1,213 @@
+<script>
+	import papaparse from 'papaparse';
+	import * as tf from '@tensorflow/tfjs';
+
+	let showCamera = true;
+	let showDetection = false;
+	let showTakePictureButton = false;
+	let showPredictButton = false;
+	let showVideoSource = false;
+	let showCanvasSource = false;
+	let showResult = false;
+	let error;
+	let videoSource = null;
+	let loading = false;
+	let secondLoading = false;
+	let image;
+	let imageCanvas;
+	let model = undefined;
+	let prediction;
+	let accuracy;
+
+	const obtainVideoCamera = async () => {
+		showVideoSource = true;
+		try {
+			showCamera = false;
+			loading = true;
+			const stream = await navigator.mediaDevices.getUserMedia({
+				video: true
+			});
+			videoSource.srcObject = stream;
+			videoSource.play();
+			loading = false;
+			showTakePictureButton = true;
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	async function loadImage() {
+		showCanvasSource = true;
+		loading = true;
+		const camera = await tf.data.webcam(videoSource);
+		image = await camera.capture();
+		showTakePictureButton = false;
+		showCamera = false;
+		showVideoSource = false;
+		await tf.browser.toPixels(image, imageCanvas);
+		image = tf.expandDims(tf.image.resizeBilinear(image, [150, 150]).div(tf.scalar(255)), 0);
+		loading = false;
+		showPredictButton = true;
+	}
+
+	async function predictWebcam() {
+		showPredictButton = false;
+		secondLoading = true;
+		tf.loadLayersModel('https://bird-ml-server.vercel.app/api/model.json').then((loadedModel) => {
+			model = loadedModel;
+			model.summary();
+			const predictionData = model.predict(image);
+			predictionData.data().then((arr) => {
+				let max = arr[0];
+				let maxIndex = 0;
+				for (let i = 0; i < arr.length; i++) {
+					if (arr[i] >= max) {
+						maxIndex = i;
+						max = arr[i];
+					}
+				}
+				accuracy = Math.round(max * 100);
+				papaparse.parse('https://bird-ml-server.vercel.app/api/listOfBirdSpecies.csv', {
+					download: true,
+					complete: (results) => {
+						prediction = results.data[maxIndex][0];
+					}
+				});
+				secondLoading = false;
+				showDetection = true;
+			});
+		});
+	}
+
+	async function logData() {
+		const response = await fetch('https://bird-ml-server.vercel.app/api/logging', {
+			method: 'POST',
+			body: prediction,
+			headers: {
+				'content-type': 'text/plain',
+				accept: 'text/plain'
+			}
+		}).then((res) => {
+			console.log(res.json());
+		});
+
+		clearScreen();
+		showResult = true;
+		error = response;
+	}
+
+	// Helper function
+	function clearScreen() {
+		showDetection = false;
+		showCamera = false;
+		showVideoSource = false;
+		showCanvasSource = false;
+		showTakePictureButton = false;
+		showPredictButton = false;
+	}
+</script>
+
+<div
+	class="w-max mt-12 ml-auto mr-auto flex-initial justify-center content-center px-12 text-center py-2"
+>
+	<div class="text-center justify-center">
+		{#if loading}
+			<div class="mt-12 alert alert-warning shadow-lg">
+				<div>
+					<strong>LOADING...</strong>
+				</div>
+			</div>
+		{/if}
+		<!-- svelte-ignore a11y-media-has-caption -->
+		{#if showVideoSource}
+			<video bind:this={videoSource} />
+		{/if}
+		{#if showCanvasSource}
+			<canvas bind:this={imageCanvas} />
+		{/if}
+		{#if showCamera}
+			<div class="card w-96 bg-base-100 shadow-xl">
+				<figure><img src="hundredpicture.jpg" alt="Shoes" /></figure>
+				<div class="card-body">
+					<h2 class="card-title">Identify Bird Species</h2>
+					<p class="text-left">
+						BSABirds will automatically identify a bird picture taken on your camera!
+					</p>
+					<div class="card-actions justify-center pt-2">
+						<button class="btn-primary btn" on:click={obtainVideoCamera}>Turn on Camera</button>
+					</div>
+				</div>
+			</div>
+		{/if}
+	</div>
+	<div>
+		{#if showTakePictureButton}
+			<button class="btn mt-2" on:click={loadImage}>Take a picture</button>
+		{/if}
+		{#if showPredictButton}
+			<button class="btn btn-primary mt-2" on:click={predictWebcam}>Predict Bird</button>
+		{/if}
+		{#if secondLoading}
+			<div class="mt-12 alert alert-warning shadow-lg">
+				<div>
+					<strong>LOADING...</strong>
+				</div>
+			</div>
+		{/if}
+	</div>
+	{#if showDetection}
+		<div class="text-center justify-center ml-auto mr-auto mt-8 card w-96 bg-base-100 shadow-xl">
+			<div class="card w-96 bg-secondary">
+				<div class="card-body items-center text-center">
+					<h2 class="card-title">Bird Species:</h2>
+					<p><b>{prediction}</b> with {accuracy}% certainty!</p>
+					<div class="mt-2 space-x-1 card-actions justify-end">
+						<button on:click={logData} class="btn btn-primary">Log Bird</button>
+						<a href="/">
+							<button class="btn btn-neutral">Try Again</button>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	{/if}
+	{#if showResult}
+		{#if error}
+			<div class="alert alert-error shadow-lg">
+				<div>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="stroke-current flex-shrink-0 h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+						/></svg
+					>
+					<span>Error! Something went wrong...</span>
+				</div>
+			</div>
+		{:else}
+			<div class="alert alert-success shadow-lg">
+				<div>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="stroke-current flex-shrink-0 h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+						/></svg
+					>
+					<span>Bird saved in databse succesfully!</span>
+				</div>
+			</div>
+		{/if}
+	{/if}
+</div>
