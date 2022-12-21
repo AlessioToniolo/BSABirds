2dac937cd37db38fbad1135a4c07293096db9e66
AlessioToniolo <atoniolo76@gmail.com>
2022-12-20 19:49:08 -0500

    Svelte Update Refactor

---
+++ src/routes/start/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<div class="mt-12 ml-auto mr-auto">
+	<div class="flex w-full mt-full">
+		<div class="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">content</div>
+		<div class="divider divider-horizontal">OR</div>
+		<div class="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">content</div>
+	</div>
+</div>
