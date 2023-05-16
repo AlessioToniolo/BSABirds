bbabe5a7768fd235a684362470b0ce2eb6bcf1d1
Alessio Toniolo <44009231+AlessioToniolo@users.noreply.github.com>
2023-05-16 11:37:04 +0000

    Analytics Tracking

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,11 @@
 	import '../app.css';
 	import Footer from '../components/Footer.svelte';
 	import Navbar from '../components/Navbar.svelte';
+
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+ 
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="container max-w-screen-lg flex-col flex h-screen justify-between">
